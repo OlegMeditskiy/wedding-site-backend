d5181de507d00a2de34e3bd367f46a5ed175cb05
OlegMeditskiy <olegmeditsky@gmail.com>
2020-07-18 21:00:42 +0300

    .

--- src/main/java/weddingsitebackend/weddingsitebackend/repository/siteObjects/PersonalInvitationRepo.java
+++ src/main/java/weddingsitebackend/weddingsitebackend/repository/siteObjects/PersonalInvitationRepo.java
@@ -8,5 +8,4 @@ import java.util.UUID;
 
 @Repository
 public interface PersonalInvitationRepo extends JpaRepository<PersonalInvitation, Long> {
-    PersonalInvitation findByUniqueKey (UUID uniqueKey);
 }
